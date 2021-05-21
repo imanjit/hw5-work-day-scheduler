94144a77c8646be63e0ec8ee54c05e393a2ccc07
Iman Bains <imanjitbains@yahoo.com>
2021-05-20 17:25:45 -0700

    Added function to color code timeblocks based on current time

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+var timeNow = parseInt(moment().format('HH'));
 var todaysDate = moment().format("[Today is] LLLL");
 $("#currentDay").append(todaysDate);
+
+$(document).ready(function() {
+    $("input").each(function() {
+        var timeName = parseInt($(this).attr("name"));
+        if (timeName === timeNow) {
+            $(this).addClass("present");
+        }
+        else if (timeName < timeNow) {
+            $(this).removeClass('present')
+            $(this).addClass("past");
+        }
+        else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass("future");
+        }
+    })
+});
